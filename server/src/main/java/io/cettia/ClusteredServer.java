0298b4b961691d32ec1a0362dcf09e569150fad8
Donghwan Kim <flowersinthesand@gmail.com>
2018-10-03 17:21:32 +0900

    Intercept the find method call only
    Fixes #42

--- server/src/main/java/io/cettia/ClusteredServer.java
+++ server/src/main/java/io/cettia/ClusteredServer.java
@@ -19,90 +19,33 @@ import io.cettia.asity.action.Action;
 import io.cettia.asity.action.Actions;
 import io.cettia.asity.action.ConcurrentActions;
 
-import java.io.Serializable;
 import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
- * {@link Server} implementation for clustering.
- * <p/>
- * With this implementation, {@code server.all(action)} have {@code action} be executed with
- * every socket in every server in the cluster.
- * <p/>
- * This implementation adopts the publish and subscribe model from Java Message Service to
- * support clustering. Here, the exchanged message represents method invocation to be executed by
- * every server in the cluster. The following methods create such messages.
- * <ul>
- * <li>{@link Server#all()}</li>
- * <li>{@link Server#all(Action)}</li>
- * <li>{@link Server#byTag(String...)}</li>
- * <li>{@link Server#byTag(String, Action)}</li>
- * <li>{@link Server#byTag(String[], Action)}</li>
- * </ul>
- * A message created by this server is passed to {@link ClusteredServer#onpublish(Action)} and a
- * message created by other servers is expected to be passed to
- * {@link ClusteredServer#messageAction()}. Therefore, what you need to do is to publish a
- * message given through {@link ClusteredServer#onpublish(Action)} to every server in the cluster
- * and to subscribe a published message by other servers to delegate it to
- * {@link ClusteredServer#messageAction()}.
- * <p/>
- * Accordingly, such message must be able to be serialized and you have to pass
- * {@link Action} implementing {@link Serializable}. However, serialization of inner classes
- * doesn't work in some cases as expected so that always use {@link Sentence} instead of action
- * if possible unless you use lambda expressions.
+ * {@link Server} implementation to support clustered environments.
  *
+ * @see <a href="https://cettia.io/guides/cettia-tutorial/#scaling-a-cettia-application">Scaling a Cettia Application</a>
  * @author Donghwan Kim
- * @see <a
- * href="http://docs.oracle.com/javase/7/docs/platform/serialization/spec/serial-arch
- * .html#4539">Note
- * of the Serializable Interface</a>
  */
 public class ClusteredServer extends DefaultServer {
 
   private Actions<Map<String, Object>> publishActions = new ConcurrentActions<>();
   private Action<Map<String, Object>> messageAction = map -> {
-    String methodName = (String) map.get("method");
-    Object[] args = (Object[]) map.get("args");
-    switch (methodName) {
-      case "find":
-        ClusteredServer.super.find((ServerSocketPredicate) args[0], (SerializableAction<ServerSocket>) args[1]);
-        break;
-      case "all":
-        ClusteredServer.super.all((SerializableAction<ServerSocket>) args[0]);
-        break;
-      case "byTag":
-        ClusteredServer.super.byTag((String[]) args[0], (SerializableAction<ServerSocket>) args[1]);
-        break;
-      default:
-        throw new IllegalArgumentException("Illegal method name in processing message: "
-          + methodName);
-    }
+    ServerSocketPredicate predicate = (ServerSocketPredicate) map.get("predicate");
+    SerializableAction<ServerSocket> action = (SerializableAction<ServerSocket>) map.get("action");
+    ClusteredServer.super.find(predicate, action);
   };
 
   @Override
   public Server find(ServerSocketPredicate predicate, SerializableAction<ServerSocket> action) {
-    publishMessage("find", predicate, action);
-    return this;
-  }
-
-  @Override
-  public Server all(SerializableAction<ServerSocket> action) {
-    publishMessage("all", action);
-    return this;
-  }
-
-  @Override
-  public Server byTag(String[] names, SerializableAction<ServerSocket> action) {
-    publishMessage("byTag", names, action);
-    return this;
-  }
-
-  private void publishMessage(String method, Object... args) {
     Map<String, Object> map = new LinkedHashMap<>();
-    map.put("method", method);
-    map.put("args", args);
+    map.put("predicate", predicate);
+    map.put("action", action);
+
     publishActions.fire(Collections.unmodifiableMap(map));
+    return this;
   }
 
   /**
