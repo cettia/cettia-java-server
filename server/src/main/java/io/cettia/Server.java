4cafdc2b26b84f075ac2bc8028e38dcf5cf196a5
Donghwan Kim <flowersinthesand@gmail.com>
2018-10-03 15:56:54 +0900

    Deprecate finders in favor of ServerSocketPredicates
    Fixes #41

--- server/src/main/java/io/cettia/Server.java
+++ server/src/main/java/io/cettia/Server.java
@@ -38,45 +38,76 @@ public interface Server extends Action<ServerTransport> {
   Server find(ServerSocketPredicate predicate, SerializableAction<ServerSocket> action);
 
   /**
-   * Returns a sentence that every socket in this server has to follow.
+   * Adds a socket event handler to be called when the socket has been created in this server.
    */
+  Server onsocket(Action<ServerSocket> action);
+
+  /**
+   * Returns a sentence with a predicate that always matches.
+   *
+   * @deprecated As of 1.2, it's deprecated in favor of {@link ServerSocketPredicates}. Use
+   * {@link Server#find(ServerSocketPredicate)} with {@link ServerSocketPredicates#all()}, i.e.
+   * <code>server.find(ServerSocketPredicates.all())</code>.
+   */
+  @Deprecated
   default Sentence all() {
     return new Sentence(this, ServerSocketPredicates.all());
   }
 
   /**
-   * Executes the given action retrieving every socket in this server.
+   * Executes the given action through a sentence returned by {@link Server#all()}.
+   *
+   * @deprecated As of 1.2, it's deprecated in favor of {@link ServerSocketPredicates}. Use
+   * {@link Server#find(ServerSocketPredicate)} with {@link ServerSocketPredicates#all()} and
+   * {@link Sentence#execute(SerializableAction)}, i.e. <code>server.find(ServerSocketPredicates.all
+   * ()).execute(action)</code>.
    */
+  @Deprecated
   default Server all(SerializableAction<ServerSocket> action) {
     all().execute(action);
     return this;
   }
 
   /**
-   * Returns a sentence that the socket tagged with the given tags in this server have to follow.
+   * Returns a sentence with a predicate that tests the socket tags against the given tags.
+   *
+   * @deprecated As of 1.2, it's deprecated in favor of {@link ServerSocketPredicates}. Use
+   * {@link Server#find(ServerSocketPredicate)} with
+   * {@link ServerSocketPredicates#tag(String...)}, i.e.
+   * <code>server.find(ServerSocketPredicates.tag(names))</code>.
    */
+  @Deprecated
   default Sentence byTag(String... names) {
     return new Sentence(this, ServerSocketPredicates.tag(names));
   }
 
   /**
-   * Executes the given action retrieving the socket tagged with the given tag in this server.
+   * Executes the given action through a sentence returned by {@link Server#byTag(String...)}.
+   *
+   * @deprecated As of 1.2, it's deprecated in favor of {@link ServerSocketPredicates}. Use
+   * {@link Server#find(ServerSocketPredicate)} with
+   * {@link ServerSocketPredicates#tag(String...)} and
+   * {@link Sentence#execute(SerializableAction)}, i.e. <code>server.find(ServerSocketPredicates
+   * .tag(name)).execute(action)</code>.
    */
+  @Deprecated
   default Server byTag(String name, SerializableAction<ServerSocket> action) {
     return byTag(new String[]{name}, action);
   }
 
   /**
-   * Executes the given action retrieving the socket tagged with the given tags in this server.
+   * Executes the given action through a sentence returned by {@link Server#byTag(String...)}.
+   *
+   * @deprecated As of 1.2, it's deprecated in favor of {@link ServerSocketPredicates}. Use
+   * {@link Server#find(ServerSocketPredicate)} with
+   * {@link ServerSocketPredicates#tag(String...)} and
+   * {@link Sentence#execute(SerializableAction)}, i.e. <code>server.find(ServerSocketPredicates
+   * .tag(names)).execute(action)</code>.
    */
+  @Deprecated
   default Server byTag(String[] names, SerializableAction<ServerSocket> action) {
     byTag(names).execute(action);
     return this;
   }
-
-  /**
-   * Adds a socket event handler to be called when the socket has been created in this server.
-   */
-  Server onsocket(Action<ServerSocket> action);
 
 }
